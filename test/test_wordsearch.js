9ea58a3f891c5050fa234116073e28dc69b4bec3
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 21:13:22 -0700

    Fix: short words now supported

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,6 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+  
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -15,12 +16,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'RJTEFEA')
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +33,54 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'F', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present, but reversed horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is present, but reversed vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'A', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'R', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'F', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,78 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+// A word search in a nested arrays of letters
+const wordSearch = (letters, word) => {
+  // Search horizontally for a match
+  if (wordSearchHorizontal(letters, word)) return true;
+  // Search vertically for a match
+  if (wordSearchVertical(letters, word)) return true;
+  return false;
 }
+
+// A horizontal word search in a nested arrays of letters
+const wordSearchHorizontal = (letters, word) => {
+  if (typeof word !== 'string') return false;
+  // Join the elements of the nested arrays into words
+  const joinedNestedArrays = horizontalJoin(letters);
+  // Search horizontally for a match
+  if (foundInArray(joinedNestedArrays, word)) return true;
+  // Search horizontally for a match of the reversed word
+  const reversedWordToFind = reverseString(word);
+  if (foundInArray(joinedNestedArrays, reversedWordToFind)) return true;
+  return false;
+}
+
+// A horizontal word search in a nested arrays of letters
+const wordSearchVertical = (letters, word) => {
+  if (typeof word !== 'string') return false;
+  // Join the elements of the nested arrays into words
+  const joinedNestedArrays = horizontalJoin(letters);
+  // Transpose the array, and then we can search horizontally for a match
+  const transposedArray = transpose(joinedNestedArrays);
+  // Search horizontally for a match
+  if (wordSearchHorizontal(transposedArray, word)) return true;
+  return false;
+}
+
+// Searches for a string within array elements
+const foundInArray = (array, stringToFind, caseSensitive = false) => {
+  for (const element of array) {
+    if (foundInString(element, stringToFind, caseSensitive)) return true;
+  }
+  return false;
+}
+
+// Searches for a string within a string
+const foundInString = (lookInString, stringToFind, caseSensitive = false) => {
+  const regExpFlags = '';
+  if (caseSensitive) regExpFlags += 'i';
+  const match = lookInString.match(stringToFind, regExpFlags);
+  const wasFound = Array.isArray(match);
+  return wasFound;
+}
+
+// Joins elements of nested arrays to form a single word per array
+const horizontalJoin = (array) => array.map(element => element.join(''));
+
+const reverseString = (string) => {
+  let result = '';
+  for (let letter = string.length-1; letter >= 0; letter --) {
+    result += string[letter];
+  }
+  return result;
+}
+
+// A function that will return a transposed version of a given 2D array
+const transpose = function(matrix) {
+  let output = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    // Create an empty row for each row in the matrix
+    output.push([]);
+  }
+  for (const row in matrix) {
+    for (const col in matrix[row]) {
+      output[col][row] = matrix[row][col];
+    }
+  }
+  return output;
+};
 
 module.exports = wordSearch
