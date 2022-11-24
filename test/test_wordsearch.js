feeebfaf018eeb0806fc44d36f6444d88d1094ba
Mark Bieganek <mark.bieganek@telus.com>
2022-11-23 21:32:31 -0700

    Handle edge cases, empty args, min word length 2

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,22 +4,6 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'RJTEFEA')
-
-    assert.isFalse(result);
-  });
 
   it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
@@ -83,5 +67,59 @@ describe("#wordSearch()", function() {
     ], 'FRANK')
 
     assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BUFFALO')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is less than 2 characters", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'A', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'R', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'F', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'A')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not provided", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'A', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'R', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'F', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
+  
+  it("should return false if the letters array is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,5 +1,8 @@
 // A word search in a nested arrays of letters
 const wordSearch = (letters, word) => {
+  if (Array.isArray(letters) === false) return false;
+  if (typeof word !== 'string') return false;
+  if (letters.length === 0 || word.length <= 1) return false;
   // Search horizontally for a match
   if (wordSearchHorizontal(letters, word)) return true;
   // Search vertically for a match
@@ -9,7 +12,9 @@ const wordSearch = (letters, word) => {
 
 // A horizontal word search in a nested arrays of letters
 const wordSearchHorizontal = (letters, word) => {
+  if (Array.isArray(letters) === false) return false;
   if (typeof word !== 'string') return false;
+  if (letters.length === 0 || word.length <= 1) return false;
   // Join the elements of the nested arrays into words
   const joinedNestedArrays = horizontalJoin(letters);
   // Search horizontally for a match
@@ -22,7 +27,9 @@ const wordSearchHorizontal = (letters, word) => {
 
 // A horizontal word search in a nested arrays of letters
 const wordSearchVertical = (letters, word) => {
+  if (Array.isArray(letters) === false) return false;
   if (typeof word !== 'string') return false;
+  if (letters.length === 0 || word.length <= 1) return false;
   // Join the elements of the nested arrays into words
   const joinedNestedArrays = horizontalJoin(letters);
   // Transpose the array, and then we can search horizontally for a match
@@ -34,6 +41,9 @@ const wordSearchVertical = (letters, word) => {
 
 // Searches for a string within array elements
 const foundInArray = (array, stringToFind, caseSensitive = false) => {
+  if (Array.isArray(array) === false) return false;
+  if (typeof stringToFind !== 'string') return false;
+  if (array.length === 0 || stringToFind.length === 0) return false;
   for (const element of array) {
     if (foundInString(element, stringToFind, caseSensitive)) return true;
   }
@@ -42,6 +52,8 @@ const foundInArray = (array, stringToFind, caseSensitive = false) => {
 
 // Searches for a string within a string
 const foundInString = (lookInString, stringToFind, caseSensitive = false) => {
+  if (typeof lookInString !== 'string' || typeof stringToFind !== 'string') return false;
+  if (lookInString.length === 0 || stringToFind.length === 0) return false;
   const regExpFlags = '';
   if (caseSensitive) regExpFlags += 'i';
   const match = lookInString.match(stringToFind, regExpFlags);
